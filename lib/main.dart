1ba60e3a4c7aa66a44a9d6fad7d432cc8dc3276f
aya-meggally <ayameggaly10@gmail.com>
2024-08-21 17:47:35 +0300

    Finish home ui and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo3/app_theme.dart';
+import 'package:todo3/firebase_options.dart';
 import 'package:todo3/home_screen.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp( ToDoRoute());
 }
 
